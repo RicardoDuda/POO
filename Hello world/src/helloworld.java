f4ba0b3c47742e7d4b1e620f1d8af278c26c13a2
a1520806 <a1520806@L2-09.hto.ifsp.edu.br>
2017-08-01 21:25:35 -0300

    Aula Inicial POO!

---
+++ Hello world/src/helloworld.java
@@ -0,0 +1,10 @@
+
+public class helloworld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
+
