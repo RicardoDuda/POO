d132c8140f29cbed9ea3d0e3dd067b77063333f7
a1520806 <a1520806@L2-09.hto.ifsp.edu.br>
2017-08-01 21:34:21 -0300

    Nova Mensagem de tela!

--- Hello world/src/helloworld.java
+++ Hello world/src/helloworld.java
@@ -4,6 +4,7 @@ public class helloworld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        System.out.println("Aula Inicial POO");
     }
 
 }
